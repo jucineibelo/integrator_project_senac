a1cc87f31aa2f39f3d4e304c2018222c597733b1
jucineibelo <jucineibelo@gmail.com>
2025-06-30 19:04:44 -0300

    Feat: implementation first entity

---
+++ projeto/tecnoos/src/main/java/com/senac/tecnoos/adapter/persistence/MySQLConnectionFactory.java
@@ -0,0 +1,16 @@
+package com.senac.tecnoos.adapter.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnectionFactory implements DataBaseConnectionFactory{
+
+    private static final String URL = "jdbc:mysql://localhost:3306/base";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    @Override
+    public Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
