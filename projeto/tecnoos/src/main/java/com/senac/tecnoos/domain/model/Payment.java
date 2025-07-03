02815a2397d15ed99babac05ae95fab1692a09e4
JUCINEI LUIZ BELO <jucinei.belo@alunos.sc.senac.br>
2025-07-02 21:57:52 -0300

    implementado fxml e ajuste de dependencias

---
+++ projeto/tecnoos/src/main/java/com/senac/tecnoos/domain/model/Payment.java
@@ -0,0 +1,33 @@
+package com.senac.tecnoos.domain.model;
+
+import java.util.Date;
+
+public class Payment {
+    private Long id;
+    private String description;
+    private Date registrationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
