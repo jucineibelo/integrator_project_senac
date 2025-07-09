d89a7893760f43a442998a92d95b58838c94727a
LUCAS GUILHERME SCHMIDT <lucas.schmidt@alunos.sc.senac.br>
2025-07-09 19:56:16 -0300

    Metodos Crud Service

--- projeto/tecnoos/src/main/java/com/senac/tecnoos/domain/gateway/ServiceGateway.java
+++ projeto/tecnoos/src/main/java/com/senac/tecnoos/domain/gateway/ServiceGateway.java
@@ -1,4 +1,15 @@
 package com.senac.tecnoos.domain.gateway;
 
+import com.senac.tecnoos.domain.model.Payment;
+import java.util.List;
+
 public interface ServiceGateway {
+
+    void save(Payment payment);
+
+    List<Payment> getPayments();
+
+    void update(Payment payment);
+
+    void delete(Long id);
 }
